00186f083795b780e53bf100199dc74c81efef76
AnitaOwen <anitaowen@pursuit.org>
2024-02-28 15:55:09 -0500

    updated item name key in the model

--- models/transaction.model.js
+++ models/transaction.model.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     id: 1,
-    item_name: "Salary",
+    itemName: "Salary",
     amount: 3000,
     date: "2024-02-01",
     from: "Employer",
@@ -10,7 +10,7 @@ module.exports = [
   },
   {
     id: 2,
-    item_name: "Rent",
+    itemName: "Rent",
     amount: -1200,
     date: "2024-02-05",
     from: "Landlord",
@@ -19,7 +19,7 @@ module.exports = [
   },
   {
     id: 3,
-    item_name: "Groceries",
+    itemName: "Groceries",
     amount: -150,
     date: "2024-02-10",
     from: "Grocery Store",
@@ -28,7 +28,7 @@ module.exports = [
   },
   {
     id: 4,
-    item_name: "Savings",
+    itemName: "Savings",
     amount: 500,
     date: "2024-02-15",
     from: "Bank",
@@ -37,7 +37,7 @@ module.exports = [
   },
   {
     id: 5,
-    item_name: "Utility Bill",
+    itemName: "Utility Bill",
     amount: -100,
     date: "2024-02-20",
     from: "Utility Company",
@@ -46,7 +46,7 @@ module.exports = [
   },
   {
     id: 6,
-    item_name: "Dinner with Friends",
+    itemName: "Dinner with Friends",
     amount: -50,
     date: "2024-02-25",
     from: "Restaurant",
@@ -55,7 +55,7 @@ module.exports = [
   },
   {
     id: 7,
-    item_name: "Freelance Gig",
+    itemName: "Freelance Gig",
     amount: 600,
     date: "2024-02-03",
     from: "Client",
@@ -64,7 +64,7 @@ module.exports = [
   },
   {
     id: 8,
-    item_name: "Internet Bill",
+    itemName: "Internet Bill",
     amount: -50,
     date: "2024-02-08",
     from: "Internet Provider",
@@ -73,7 +73,7 @@ module.exports = [
   },
   {
     id: 9,
-    item_name: "New Phone",
+    itemName: "New Phone",
     amount: -800,
     date: "2024-02-12",
     from: "Electronics Store",
@@ -82,7 +82,7 @@ module.exports = [
   },
   {
     id: 10,
-    item_name: "Birthday Gift",
+    itemName: "Birthday Gift",
     amount: -30,
     date: "2024-02-18",
     from: "Gift Shop",
