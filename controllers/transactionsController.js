89895f1b5c03ce63fca388587a9fc2b743fedf71
AnitaOwen <anitaowen@pursuit.org>
2024-02-28 11:09:33 -0500

    added routes for index and single transaction

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -1,9 +1,16 @@
 const express = require("express")
+
 const transactions = express.Router()
 let transactionsArray = require("../models/transaction.model.js")
 
 transactions.get("/", (req, res) => {
     res.json({ transactions: transactionsArray })
+})
+
+transactions.get("/:id", (req, res) => {
+    const { id } = req.params
+    const transaction = transactionsArray.find((transaction) => transaction.id === +id)
+    res.json({ transaction })
 })
 
 module.exports = transactions;
